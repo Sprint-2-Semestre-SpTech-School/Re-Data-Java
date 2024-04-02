d40730e5ea086bdccfef1bb608d1c50bc877b56f
JuliaIFreitas <julia.freitas@sptech.school>
2024-04-02 19:22:02 -0300

    Login em java com POO

--- login-projeto-sprint1/src/main/java/codigoLogin/Login.java
+++ login-projeto-sprint1/src/main/java/codigoLogin/Login.java
@@ -5,9 +5,11 @@ import java.util.Scanner;
 public class Login {
     public static void main(String[] args) {
 
-        Metodos validarLogin = new Metodos();
+        Metodos validarLogin = new Metodos("fernanda.caramico","SPtechPI");
 
         validarLogin.validacaoLogin();
+
+        System.out.println(validarLogin);
 
     }
 }
