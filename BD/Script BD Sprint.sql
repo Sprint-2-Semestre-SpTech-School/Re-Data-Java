063b1f50131470c5155b94a3c7202b789df03764
ArthsUrs <arthur.silva@sptech.school.com>
2024-05-16 08:21:57 -0300

    alteracao - banco de dados - Script

--- BD/Script BD Sprint.sql
+++ BD/Script BD Sprint.sql
@@ -24,61 +24,6 @@ CREATE TABLE IF NOT EXISTS `projeto` (
     REFERENCES `empresa` (`idEmpresa`)
     );
 
-<<<<<<< HEAD
-=======
-create table if not exists DispositivosUSB
-(deviceId int primary key auto_increment,
-descricao varchar(45));
-	
-create table if not exists Blacklist
-(idBlacklist int,
-statusBloqueio tinyint,
-motivoBloqueio varchar(250),
-fkDeviceId int,
-fkMaquina int,
-fkProjeto int,
-fkEmpresa int,
-primary key (idBlacklist, fkDeviceId, fkMaquina, fkProjeto, fkEmpresa),
-    foreign key (fkDeviceId) references DispositivosUSB (deviceId),
-	foreign key (fkMaquina) references Maquina (idMaquina),
-    foreign key (fkProjeto) references Projeto (idProjeto),
-    foreign key (fkEmpresa) references Empresa (idEmpresa));
-    
-    
-create table if not exists infoHardware
-(codHardware int primary key auto_increment,
-nomeCpu varchar(150),
-memoriaTotalRam int,
-nomeDisco varchar(150),
-nomeRede varchar(150),
-fkMaquina int,
-	constraint fkMaquinaHardware foreign key (fkMaquina) references Maquina (idMaquina));
-    
-create table if not exists `Cpu`
-(idCpu int primary key auto_increment,
-frequencia double,
-numeroCpuFisica int,
-numeroCpuLogica int,
-numeroPacote int,
-usoDeCpu double,
-tempoCapturas datetime,
-fkCodHardware int,
-	constraint fkHardwareCpu foreign key (fkCodHardware) references infoHardware (codHardware),
-fkMaquina int,
-	constraint fkMaquinaCpu foreign key (fkMaquina) references Maquina (idMaquina)
-);
-    
-create table if not exists Ram
-(idRam int primary key auto_increment,
-memoriaDisponivel double,
-memoriaEmUso double,
-tempoCapturas datetime,
-fkCodHardware int,
-	constraint fkHardwareRam foreign key (fkCodHardware) references infoHardware (codHardware),
-fkMaquina int,
-	constraint fkMaquinaRam foreign key (fkMaquina) references Maquina (idMaquina)
-);
->>>>>>> refs/remotes/origin/dev
 
 CREATE TABLE IF NOT EXISTS `maquina` (
   `idMaquina` INT NOT NULL,
