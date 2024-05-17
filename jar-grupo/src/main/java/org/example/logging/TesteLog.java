5312d1cdba9950d98fd2f69529859b7fe3b576c9
Sabrina <sabrina.tulio@sptech.school>
2024-05-17 17:54:40 -0300

    Consertando a importação da classe Gerador na classe App.

--- jar-grupo/src/main/java/org/example/logging/TesteLog.java
+++
@@ -1,9 +0,0 @@
-package logging;
-
-public class TesteLog {
-    public static void main(String[] args) {
-        GeradorLog.log("Iniciando a aplicação");
-
-        GeradorLog.log("Aplicação finalizada.");
-    }
-}
